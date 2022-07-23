93331254863fbd30c7b390135e87b18db0689260
brucealberto <brucealberto2@gmail.com>
2022-07-23 11:31:30 -0300

    feat: exercise 1

---
+++ 01.operacoesAritmeticas.js
@@ -0,0 +1,7 @@
+const num1 = 2;
+const num2 = 4;
+
+console.log(`Soma: ${num1} + ${num2} = ${num1 + num2}`);
+console.log(`Subtração: ${num1} - ${num2} = ${num1 - num2}`);
+console.log(`Divisão: ${num1} / ${num2} = ${num1 / num2}`);
+console.log(`Multiplicação: ${num1} * ${num2} = ${num1 * num2}`);
