487962f7b0d224c2bad9da3e1d6d936630d4eaf3
brucealberto <brucealberto2@gmail.com>
2022-08-05 01:02:06 -0300

    feat: exercicio 14

---
+++ 14.conversorFusoHorario.js
@@ -0,0 +1,8 @@
+
+let horaBrasil = 23;
+let horaFranca = horaBrasil + 5;
+
+let conversaoHora = (horaFranca > 24) ? horaFranca - 24 : horaFranca;
+
+console.log(`A hora agora no Brasil é ${horaBrasil} hrs, e a hora na França é ${conversaoHora}hrs`)
+
